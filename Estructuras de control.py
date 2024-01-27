27b353166230211515505c593cecabf089910899
alnardosdiary <infernoar666@gmail.com>
2024-01-27 19:39:44 -0300

    si

---
+++ Estructuras de control.py
@@ -0,0 +1,34 @@
+#Ejercicio.1
+"""Numero= int(input("Ingrese su numero de cliente: "))
+
+if (Numero==1000):
+    print("¡Eres el ganador!")
+else:
+    print("Sigue intentando")"""
+
+#Ejercicio.2
+"""Numero1= int(input("Ingresar 1er numero: "))
+Numero2= int(input("Ingresar 2do numero: "))
+
+if (Numero1<Numero2):
+    print(f"El menor es: {Numero1}")
+elif (Numero2<Numero1):
+    print(f"El menor es: {Numero2}")"""
+
+#Ejercicio.3
+"""Numero1= int(input("Ingresar 1er numero: "))
+Numero2= int(input("Ingresar 2do numero: "))"""
+
+"""if (Numero1<Numero2):
+    print(f"El menor es: {Numero1}")
+elif (Numero2<Numero1):
+    print(f"El menor es: {Numero2}")
+else:
+    print("Son iguales")"""
+
+
+#Ejercicio.3
+
+usuario: input("¿Quiere terminar la ejecucion?")
+
+
